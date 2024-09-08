5e15dcf77fe38adb85c79add50d8b83d6c85a5b1
Nikolay Vyahhi <nikolay.vyahhi@hyperskill.org>
2024-09-08 18:23:22 -0400

    add better prompt add gemini

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import sys
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 from github import Github
 from github import Auth
@@ -18,13 +19,16 @@ def generate_landing(idea: str) -> str:
     g = Github(auth=auth)
     repo = g.get_repo('landing-page-generator/landing-page-generator.github.io')
 
-    openai_api_key = os.environ['OPENAI_API_KEY']
-    openai_model = os.environ.get('OPENAI_MODEL', 'gpt-4o-2024-05-13')
-    llm = ChatOpenAI(model=openai_model, api_key=openai_api_key, temperature=0.6)
-    print('AI model:', openai_model)
+    # openai_api_key = os.environ['OPENAI_API_KEY']
+    # openai_model = os.environ.get('OPENAI_MODEL', 'gpt-4o-2024-05-13')
+    # llm = ChatOpenAI(model=openai_model, api_key=openai_api_key, temperature=0.6)
+    gemini_api_key = os.environ['GEMINI_API_KEY']
+    gemini_model = os.environ.get('GEMINI_MODEL', 'gemini-1.5-flash')
+    llm = ChatGoogleGenerativeAI(model=gemini_model, api_key=gemini_api_key, temperature=0.6)
+    print('AI model:', gemini_model)
 
-    prompt = Path('prompts/first-version.txt').read_text()
-    prompt = prompt.format(idea=idea)
+    prompt = Path('prompts/second-version1.txt').read_text() + f'\n{idea}\n' + Path('prompts/second-version2.txt').read_text()
+    # prompt = prompt.format(idea=idea)
 
     # call openai using langchain
     html_content = llm.invoke(prompt).content
