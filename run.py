6fb54d005f6cba376f5e08f29cb6749bad60d0f3
Nikolay Vyahhi <nikolay.vyahhi@hyperskill.org>
2024-09-08 15:35:30 -0400

    improve code

--- run.py
+++ run.py
@@ -10,14 +10,7 @@ from dotenv import load_dotenv
 _ = load_dotenv(Path(__file__).parent / '.env')
 
 
-def main():
-    # read idea from sys argv
-    if len(sys.argv) < 2:
-        print("Usage: python run.py <idea>")
-        return
-    idea = sys.argv[1]
-    print('Idea:', idea)
-
+def generate_landing(idea: str) -> str:
     # init repo
     repo_url = 'git@github.com:landing-page-generator/landing-page-generator.github.io.git'
     repo_dir = 'landing-page-generator.github.io'
@@ -27,18 +20,15 @@ def main():
     openai_api_key = os.environ['OPENAI_API_KEY']
     openai_model = os.environ.get('OPENAI_MODEL', 'gpt-4o-2024-05-13')
     llm = ChatOpenAI(model=openai_model, api_key=openai_api_key, temperature=0.6)
-
     print('AI model:', openai_model)
-    prompt = (
-        f'Create a landing page for a new product: "{idea}". '
-        'Output ONLY html, without markdown or other formats. '
-        'Add "Made with LPG" at the bottom of the page with a link to https://landing-page-generator.github.io.'
-    )
-    print('Prompt:', prompt)
+
+    prompt = Path('prompts/first-version.txt').read_text()
+    prompt = prompt.format(idea=idea)
 
     # call openai using langchain
     html_content = llm.invoke(prompt).content
 
+    # deploy HTML page to github pages
     now = datetime.datetime.now().timestamp()
     filename = f'{now}.html'
     filepath = os.path.join(repo_dir, filename)
@@ -54,6 +44,16 @@ def main():
     print(f'URL: https://landing-page-generator.github.io/{filename}')
     print('Please wait a minute while it\'s deployed.')
     print()
+
+
+def main():
+    # read idea from sys argv
+    if len(sys.argv) < 2:
+        print("Usage: python run.py <idea>")
+        return
+    idea = sys.argv[1]
+    print('Idea:', idea)
+    generate_landing(idea)
 
 
 if __name__ == "__main__":
